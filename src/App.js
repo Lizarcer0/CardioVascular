b3d98df1cba17e6edceb539778ed1a1f06c7b14f
Lizarcer0 <elliotcamram@gmail.com>
2024-10-10 17:02:56 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Index from './pages/Index'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Index/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
